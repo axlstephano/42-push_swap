2c2e363448e07cc00826248d2b23eb6416cead59
stephanocastillo <iamaxlstephano@gmail.com>
2024-06-25 21:16:01 +0200

    yeah

--- sources/main.c
+++ sources/main.c
@@ -6,39 +6,39 @@
 /*   By: axcastil <axcastil@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/25 18:48:16 by axcastil          #+#    #+#             */
-/*   Updated: 2024/06/25 19:20:46 by axcastil         ###   ########.fr       */
+/*   Updated: 2024/06/25 21:14:50 by axcastil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 
-void	fill_stack(int ac, char **av, t_stack **stack)
+void	check_args(int ac, char **av, t_stack **stack)
 {
-	char	**values;
+	char	**matrix;
 	int		i;
 
-	if (ac == 1)
-		exit(1);
+	if (ac < 2)
+		exit (1);
 	else if (ac == 2)
-		values = ft_split(av[1], ' ');
+		matrix = ft_split(av[1], ' ');
 	else
 	{
-		i = 1;
-		while(av[i])
-			values[i++] = av[i++];
-	}
-	while (ac--)
-	{
-		printf("%s/n", *values);
-		values--;
+		i = 0;
+		matrix = malloc(sizeof(char *) * (ac - 1));
+		while ((i < ac - 1))
+		{
+			matrix[i] = av[i + 1];
+			i++;
+		}
 	}
 }
 
 int main(int ac, char **av)
 {
-	t_stack *stack_a;
+	t_stack	*stack_a;
 	t_stack	*stack_b;
-
-	fill_stack(ac, av, &stack_a);
+	
+	check_args(ac, av, &stack_a);
+	printf("%d\n", stack_a->index);
 	return (0);
 }
