86b6dd4113d75e9cc4af1e9d9e7cfbce0062b2a9
stephanocastillo <iamaxlstephano@gmail.com>
2024-06-25 21:31:48 +0200

    main fucked

--- sources/main.c
+++ sources/main.c
@@ -6,13 +6,13 @@
 /*   By: axcastil <axcastil@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/25 18:48:16 by axcastil          #+#    #+#             */
-/*   Updated: 2024/06/25 21:14:50 by axcastil         ###   ########.fr       */
+/*   Updated: 2024/06/25 21:31:25 by axcastil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 
-void	check_args(int ac, char **av, t_stack **stack)
+/* void	check_args(int ac, char **av, t_stack **stack)
 {
 	char	**matrix;
 	int		i;
@@ -25,18 +25,39 @@ void	check_args(int ac, char **av, t_stack **stack)
 	{
 		i = 0;
 		matrix = malloc(sizeof(char *) * (ac - 1));
-		while ((i < ac - 1))
+		while (i < (ac -1))
 		{
 			matrix[i] = av[i + 1];
 			i++;
 		}
 	}
+} */
+void check_args(int ac, char **av, t_stack **stack)
+{
+    if (ac <= 1) {
+        return;
+    }
+
+    char **modified_args = malloc((ac - 1) * sizeof(char *));
+    if (modified_args == NULL) {
+        return;
+    }
+
+    for (int i = 1; i < ac; i++) {
+        modified_args[i - 1] = av[i] + 1;
+    }
+
+    for (int i = 0; i < ac - 1; i++) {
+        printf("Argumento %d: %s\n", i+1, modified_args[i]);
+    }
+
+    free(modified_args);
 }
 
 int main(int ac, char **av)
 {
 	t_stack	*stack_a;
-	t_stack	*stack_b;
+	//t_stack	*stack_b;
 	
 	check_args(ac, av, &stack_a);
 	printf("%d\n", stack_a->index);
