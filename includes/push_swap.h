396002320ea029754bd832be58ce4362f5480859
stephanocastillo <iamaxlstephano@gmail.com>
2024-07-18 19:15:06 +0200

    init_stack 80%

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -3,17 +3,17 @@
 
 # include "../lib/lib42.h"
 
-typedef struct s_stack
-{
-    int size;
-    struct s_stack *head;
-}	t_stack;
-
 typedef struct s_node
 {
     int     value;
     int     index;
     struct s_node *next;
 }   t_node;
+
+typedef struct s_stack
+{
+    int size;
+    t_node *head;
+}	t_stack;
 
 #endif
--- sources/main.c
+++ sources/main.c
@@ -6,7 +6,7 @@
 /*   By: axcastil <axcastil@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/25 18:48:16 by axcastil          #+#    #+#             */
-/*   Updated: 2024/07/06 19:17:15 by axcastil         ###   ########.fr       */
+/*   Updated: 2024/07/18 19:14:08 by axcastil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,10 +56,9 @@ int	*check_numbers(int argc, char **argv, int count)
 	numbers = (int *)ft_calloc(count, sizeof(int));
 	if (!numbers)
 		return (NULL);
-
 	while (i < argc)
 	{
-		matrix = ft_split(argv[i], ' '); //split a argumento
+		matrix = ft_split(argv[i], ' '); // split a argumento
 		if (!matrix)
 			return (NULL);
 		k = 0;
@@ -71,19 +70,61 @@ int	*check_numbers(int argc, char **argv, int count)
 	return (numbers);
 }
 
-int	main(int argc, char **argv) 
+void	fill_stack(t_stack *stack, int value)
 {
-	t_stack *stack_a;
-	t_stack *stack_b;
-	int *numbers;
-	int count;
-	int i = 0;
+	t_node	*node;
 
+	if (!(node = (t_node *)malloc(sizeof(t_node))))
+		return ;
+	node->value = value;
+	node->next = stack->head;
+	stack->head = node;
+}
+
+void	init_stack(t_stack *stack_a, t_stack *stack_b, int count, int *numbers)
+{
+	t_node	*node;
+	int		i;
+
+	stack_a->head = NULL;
+	stack_b->head = NULL;
+	stack_a->size = 0;
+	stack_b->size = 0;
+	i = count - 1; // número de posiciones, desde el último al final
+	while (i >= 0)
+		fill_stack(stack_a, numbers[i--]); // rellenar stack
+	
+}
+
+int	main(int argc, char **argv)
+{
+	t_stack	stack_a;
+	t_stack	stack_b;
+	int		*numbers;
+	int		count;
+	int		i;
+
+	i = 0;
 	count = args_len(argc, argv);
 	numbers = check_numbers(argc, argv, count);
-/* 	ft_printf("count:%d\n", count); //PRINT "COUNT"
- *//* 	while (i < count) //PRINT "NUMBER" ELEMENTS
-		ft_printf("%d\n", numbers[i++]);
- */	free(numbers);
+	init_stack(&stack_a, &stack_b, count, numbers);
+	ft_printf("count:%d\n", count); // PRINT "COUNT"
+
+	// ************PRINT "NUMBER" ELEMENTS************************
+
+	
+	//while (i < count)
+	//	ft_printf("%d\n", numbers[i++]);
+	
+
+	// ******IMPRIMIR LOS VALORES DE TODOS LOS NODOS EN STACK_ A*********
+	
+	/* while(stack_a.head)
+	{
+		printf("%d\n", stack_a.head->value);
+		stack_a.head = stack_a.head->next;
+	} */
+
+	free(numbers);
 	return (0);
 }
