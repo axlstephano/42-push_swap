9d9c6730fcaab198ceb8a04bbdfa5b6e82294803
axlstephano <axl.stephano.castillo@gmail.com>
2025-01-15 13:02:16 +0100

    error_handler in process

--- inc/push_swap.h
+++ inc/push_swap.h
@@ -23,14 +23,14 @@ typedef struct s_stack
 
 ///////////ARGS/////////////////////////
 
-int	arg_len(int ac, char **av);
+/* int	arg_len(int ac, char **av);
 int	check_duplicates(int *numbers, int len);
-int	*arg_to_array(int ac, char **av, int len);
+int	*arg_to_array(int ac, char **av, int len); */
 
 ////////STACKS/////////////////////
 
-void	fill_stack(t_stack *stack, int number);
-void	init_stack(t_stack *stack_a, t_stack *stack_b, int len, int *numbers);
+/* void	fill_stack(t_stack *stack, int number);
+void	init_stack(t_stack *stack_a, t_stack *stack_b, int len, int *numbers); */
 //void	get_tail(t_stack *stack);
 
 
@@ -57,9 +57,9 @@ void	rrr(t_stack *stack_1, t_stack *stack_2);
 
 /////////UTILS//////////////////////
 
-int		is_sorted(t_stack *stack);
+/* int		is_sorted(t_stack *stack);
 void	sort_stacks(t_stack *a, t_stack *b, int len);
-void	tiny_sort(t_stack *a);
+void	tiny_sort(t_stack *a); */
 
 /////////ALGORITHMS////////////////
 
--- src/main.c
+++ src/main.c
@@ -1,9 +1,19 @@
 #include "../inc/push_swap.h"
 
+void	argument_handler(int argc, char const *argv[])
+{
+	if (argc == 2)
+		argv = ft_split (argv[1], ' ');
+	
+}
+
 int main(int argc, char const *argv[])
 {
-	t_node *a;
-	t_node *b;
 
-	return 0;
+	// error handling
+	if (argc < 2 || (argc = 2 && argv[1][0] == '\0')) //<-- if no arguments
+		return (1);
+
+	argument_handler(argc, argv);
+	return (0);
 }
